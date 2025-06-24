7ef83c47bbd3f0c21f753d95dbb53852236f8b5b
gustavo gregorio <gustavogregorio1986@gmail.com>
2025-06-24 20:36:15 -0300

    Desenvolvimento do layout com frontend no anuglar material

--- src/app/logar/logar.component.ts
+++ src/app/logar/logar.component.ts
@@ -1,25 +1,32 @@
-import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AfterViewInit, ChangeDetectorRef, Component } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { MatOptionModule } from '@angular/material/core';
 import { MatButtonModule } from '@angular/material/button';
-import { BrowserModule } from '@angular/platform-browser';
+import { RodapeComponent } from '../rodape/rodape.component';
 
 @Component({
   selector: 'app-logar',
-  imports: [ReactiveFormsModule,MatFormFieldModule,MatInputModule,MatSelectModule,MatOptionModule,MatButtonModule],
+  imports: [FormsModule, ReactiveFormsModule,MatFormFieldModule,MatInputModule,MatSelectModule,MatOptionModule,MatButtonModule, RodapeComponent],
   templateUrl: './logar.component.html',
   styleUrl: './logar.component.css'
 })
-export class LogarComponent {
+export class LogarComponent implements AfterViewInit{
 
   loginForm = new FormGroup({
     email: new FormControl('', Validators.required),
     senha: new FormControl('', Validators.required),
     role: new FormControl('', Validators.required),
   });
+
+  constructor(private cdr: ChangeDetectorRef){}
+
+  ngAfterViewInit() {
+    // Força atualização da view depois do carregamento
+    this.cdr.detectChanges();
+  }
 
   onLogin() {
     if(this.loginForm.valid) {
