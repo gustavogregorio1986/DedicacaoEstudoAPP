ebf85f985a24a0b4b0477bb66e12813fba69f3e2
gustavo gregorio <gustavogregorio1986@gmail.com>
2025-06-24 16:57:02 -0300

    ajustando layout no angular material

--- src/app/logar/logar.component.ts
+++ src/app/logar/logar.component.ts
@@ -1,30 +1,30 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { MatOptionModule } from '@angular/material/core';
 import { MatButtonModule } from '@angular/material/button';
+import { BrowserModule } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-logar',
-  imports: [MatFormFieldModule,MatInputModule,MatSelectModule,MatOptionModule,MatButtonModule],
+  imports: [ReactiveFormsModule,MatFormFieldModule,MatInputModule,MatSelectModule,MatOptionModule,MatButtonModule],
   templateUrl: './logar.component.html',
   styleUrl: './logar.component.css'
 })
 export class LogarComponent {
 
   loginForm = new FormGroup({
-     Email: new FormControl('', Validators.required),
-     Senha: new FormControl('', Validators.required),
-     Role: new FormControl('', Validators.required) 
+    email: new FormControl('', Validators.required),
+    senha: new FormControl('', Validators.required),
+    role: new FormControl('', Validators.required),
   });
 
   onLogin() {
-    if (this.loginForm.valid) {
-    const loginData = this.loginForm.value;
-    console.log('Enviando login:', loginData);
-    // Chamar backend: this.authService.login(loginData).subscribe(...)
-   }
+    if(this.loginForm.valid) {
+      console.log('Dados do login:', this.loginForm.value);
+      // lógica do login
+    }
   }
 }
