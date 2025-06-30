3d5e086ce1aabfb7408e40e0d3eb1cddbf72b169
gustavo gregorio <gustavogregorio1986@gmail.com>
2025-06-30 15:47:05 -0300

    desenvolver o service usuario no angular no api

---
+++ src/app/services/usuario.service.ts
@@ -0,0 +1,24 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Usuario } from '../models/usuario';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsuarioService {
+
+   private apiUrl = 'https://localhost:7269/api/Usuario';
+
+   constructor(private http: HttpClient) { }
+
+   //Metodo para cadastrar usuario
+   cadastrar(usuario: Usuario): Observable<Usuario>{
+      return this.http.post<Usuario>(`${this.apiUrl}/AdicionarUsuario`,usuario);
+   }
+
+   login(usuario: { email: string; senha: string; role?: string }): Observable<{ token: string }> {
+      return this.http.post<{ token: string }>(`${this.apiUrl}/login`, usuario);
+   }
+
+}
