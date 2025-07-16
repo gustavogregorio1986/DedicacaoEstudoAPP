e2ade7bc18ab22b5c9a507a66f0ba4cec56b0c38
gustavo gregorio <gustavogregorio1986@gmail.com>
2025-07-16 11:04:16 -0300

    desenvolvimento do token, falta criar as pastas de admin e usuario

--- src/app/logar/logar.component.ts
+++ src/app/logar/logar.component.ts
@@ -1,12 +1,14 @@
-import { AfterViewInit, ChangeDetectionStrategy, ChangeDetectorRef, Component, ViewEncapsulation } from '@angular/core';
-import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AfterViewInit, ChangeDetectorRef, Component } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { MatOptionModule } from '@angular/material/core';
 import { MatButtonModule } from '@angular/material/button';
 import { RodapeComponent } from '../rodape/rodape.component';
-
+import { UsuarioService } from '../services/usuario.service';
+import { Router } from '@angular/router';
+import { jwtDecode } from 'jwt-decode';
 @Component({
   selector: 'app-logar',
   imports: [FormsModule, ReactiveFormsModule,MatFormFieldModule,MatInputModule,MatSelectModule,MatOptionModule,MatButtonModule, RodapeComponent],
@@ -15,13 +17,19 @@ import { RodapeComponent } from '../rodape/rodape.component';
 })
 export class LogarComponent implements AfterViewInit{
 
-  loginForm = new FormGroup({
-    Email: new FormControl('', Validators.required),
-    Senha: new FormControl('', Validators.required),
-    Role: new FormControl('', Validators.required),
-  });
+  loginForm!: FormGroup;
 
-  constructor(private cd: ChangeDetectorRef){ }
+  constructor(private cd: ChangeDetectorRef, 
+              private fb: FormBuilder, 
+              private router: Router, private usuarioService: UsuarioService){
+   this.loginForm = this.fb.group({
+    Email: ['', [Validators.required, Validators.email]],
+    Senha: ['', Validators.required],
+    Role: ['', Validators.required]
+     });  
+   }
+
+  
 
   ngAfterViewInit() {
       this.cd.detectChanges();
@@ -30,10 +38,41 @@ export class LogarComponent implements AfterViewInit{
     });
   }
 
-  onLogin() {
-    if(this.loginForm.valid) {
-      console.log('Dados do login:', this.loginForm.value);
-      // lógica do login
+   onLogin() {
+    if (this.loginForm.valid) {
+      const payload = {
+        email: this.loginForm.value.Email,
+        senha: this.loginForm.value.Senha,
+        role: this.loginForm.value.Role,
+      };
+
+      console.log('Payload para API:', payload);
+
+
+  this.usuarioService.login(payload).subscribe({
+    next: (response) => {
+      const token = response.token;
+
+     // decodifica o token
+     const decoded: any = jwtDecode(token);
+
+      const role = decoded.role;
+
+      if (role === 'adm') {
+        this.router.navigate(['/admin']);
+      } else if (role === 'usu') {
+        this.router.navigate(['/usuario']);
+      } else {
+        this.router.navigate(['/login']);
+      }
+    },
+    error: (error) => {
+      console.error('Erro no login:', error);
+    }
+   });
+
+    } else {
+      console.log('Form inválido:', this.loginForm.errors);
     }
   }
 }
